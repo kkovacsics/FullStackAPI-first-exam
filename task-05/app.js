9ab819722924308769e03e652eaf08d4012fe64e
kkovacsics <kalman.kovacsics@gmail.com>
2021-07-05 16:33:22 +0200

    task-05: solved

--- task-05/app.js
+++ task-05/app.js
@@ -1,13 +1,30 @@
 /**
  * 1. Töltsd be a szükséges modulokat: http és az általad írt router modul.
  */
-
+const http = require('http')
+const router = require('./router/countryRouter');
+ 
 // 2. Definiáld a port értékét 8080 -ra a port változóban.
-
+const port = 8080
 /**
  * 3. Hozz létre egy http szervert.
  * A szerver get kérés esetén meghívja a router kapcsolódó metódusát, 
  * amely válaszol a kérésre.
  */
-
+const server = http.createServer(async (req, res) => {
+  const method = req.method.toLowerCase()
+  if (router[method]) {
+    const list = await router[method]()
+    res.writeHead(200, {
+      'Content-Type': 'application/json'
+    })
+    res.end(list)
+  } else {
+    res.writeHead(404, {
+      'Content-Type': 'text/plain'
+    })
+    res.end('The method is not "get"')
+  }
+})
 // 4. Állítsd be, hogy a szerver figyelje a port változóban definiált portot.
+server.listen(port, () => console.log(`Server is listening at http://127.0.0.1:${port}`))
--- task-05/router/countryRouter.js
+++ task-05/router/countryRouter.js
@@ -4,7 +4,13 @@ const carController = require('../controller/countryController');
  * 1. Készíts egy router objektumot, ami a get kulcsnál meghívja a carController 
  * getAll nevű metódusát és string típusként visszaadja az adatok tömbjét.
  */
-
+const router = {
+  get: async () => {
+    const cars = await carController.getAll()
+    return JSON.stringify(cars, null, 2)
+  }
+}
 /**
  * 2. A module az előző pontnál elkészített router objektumot adja vissza.
  */
+module.exports = router
